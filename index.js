9eed7008131d98993b386700580526b4e9637010
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2015-11-10 17:08:34 +1100

    Add functions to index js

---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict';
+var CLIEngine = require('eslint').CLIEngine;
+var chalk = require('chalk');
+var cli;
+var report;
+var formatter;
+var format = '';
+var configOptions = {};
+
+module.exports = {
+
+  configFile: function(config) {
+    configOptions.configFile = config || '.eslintrc';
+  },
+
+  ignorePath: function(ignore) {
+    configOptions.ignorePath = ignore || '.eslintignore';
+  },
+
+  envs: function(envs) {
+    configOptions.envs = envs;
+  },
+
+  extensions: function(extns) {
+    configOptions.extensions = extns;
+  },
+
+  globals: function(globals) {
+    configOptions.globals = globals;
+  },
+
+  rulePaths: function(rulePaths) {
+    configOptions.rulePaths = rulePaths;
+  },
+
+  rules: function(rules) {
+    configOptions.rules = rules;
+  },
+
+  parser: function(parser) {
+    configOptions.parser = parser;
+  },
+
+  cache: function(cache) {
+    configOptions.cache = cache;
+  },
+
+  cacheFile: function(cacheFile) {
+    configOptions.cacheFile = cacheFile;
+  },
+
+  cacheLocation: function(cacheLocation) {
+    configOptions.cacheLocation = cacheLocation;
+  },
+
+  mochalint: function (paths, options) {
+    configOptions.configFile = configOptions.configFile || '.eslintrc';
+    cli = new CLIEngine(configOptions);
+    if (options && options.formatter) {
+      format = options.formatter;
+    }
+    describe('eslint-filter-coffee', function () {
+      paths.forEach(function (p) {
+        it('should have no errors in ' + p, function () {
+          try {
+            report = cli.executeOnFiles([ p ]);
+            formatter = cli.getFormatter(format);
+          } catch (err) {
+            throw new Error(err);
+          }
+
+          if (report && report.errorCount > 0) {
+            throw new Error(chalk.red('code did not pass lint rules') + formatter(report.results));
+          }
+
+          if (report && report.warningCount > 0) {
+            console.log(chalk.yellow('code has lint warnings'));
+            console.log(formatter(report.results));
+          }
+        });
+      });
+    });
+  }
+};
